62db63f3c4708180847c81704bdbe74318bcc572
EO4Wellness <55713583+EO4wellness@users.noreply.github.com>
2021-02-04 13:25:27 -0600

    Create cat-gif-addon.js

---
+++ Intro-to-JavaScript/cat-gif-addon.js
@@ -0,0 +1,7 @@
+document.body.addEventListener('click', function () {
+     var myParent = document.getElementsByTagName("h1")[0]; 
+     var myImage = document.createElement("img");
+     myImage.src = 'https://thecatapi.com/api/images/get?format=src&type=gif';
+     myParent.appendChild(myImage);
+     myImage.style.marginLeft = "160px";
+});
