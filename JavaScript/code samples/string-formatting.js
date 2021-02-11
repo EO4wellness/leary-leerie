612b99ea88a0661c2d4f6fd6a0410ee0aac14bbe
eo4wellness <eo4wellness1122617@gmail.com>
2021-02-10 20:26:48 -0600

    Create string-formatting.js

---
+++ JavaScript/code samples/string-formatting.js
@@ -0,0 +1,15 @@
+/*
+
+this multi-line comment gives examples of string formatting 
+
+single quote:
+
+'string-value-goes-here'
+
+double quotes:
+
+"string-value-goes-here"
+
+mixing quote styles (one single, one double) or leaving out the quotes (single or double) result in error messages 
+
+*/
