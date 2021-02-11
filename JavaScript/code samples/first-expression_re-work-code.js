513cb414bddf725f0eb5e1b65305a976757ac55f
eo4wellness <eo4wellness1122617@gmail.com>
2021-02-10 20:26:28 -0600

    Create first-expression_re-work-code.js

---
+++ JavaScript/code samples/first-expression_re-work-code.js
@@ -0,0 +1,17 @@
+/*
+ * Programming Quiz: First Expression (2-1)
+ * Write an expression that uses at least three, distinct, arithmetic operators
+ * to log the number 42 to the console.
+ */
+ 
+/*
+ * QUIZ REQUIREMENTS
+ * 1. Your code should print the value `42`
+ * 2. You should use at least 3 distinct operators. (`+`, `-`, `*`, `/`, or `%`)
+ * 3. Your code should not be empty
+ */
+
+
+// this expression equals 4, change it to equal 42
+console.log(((84 + 2) / 2 )-1);
+// my rework of the equation once i viewed their answer and understood what they actually meant by their quiz requirements 
