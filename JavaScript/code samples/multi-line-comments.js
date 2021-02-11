2100e61f07a102858188b077f7c228dd0506d6bd
eo4wellness <eo4wellness1122617@gmail.com>
2021-02-10 20:26:39 -0600

    Create multi-line-comments.js

---
+++ JavaScript/code samples/multi-line-comments.js
@@ -0,0 +1,14 @@
+/*
+comment line 1 
+comment line 2
+comment line 3 
+and so on 
+this is an example of the formatting of 
+javasScript multiline comments
+
+all such comments begin and close with a 
+
+slash-star-comment content-star-slash 
+
+formating as shown here 
+*/
