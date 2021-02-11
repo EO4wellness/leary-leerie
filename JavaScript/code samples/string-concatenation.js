94746cec5f66e0718130181d62865c4f82ea2316
eo4wellness <eo4wellness1122617@gmail.com>
2021-02-10 20:26:45 -0600

    Create string-concatenation.js

---
+++ JavaScript/code samples/string-concatenation.js
@@ -0,0 +1,26 @@
+/*
+
+Concatenation of Strings
+
+it isn't math 
+
+it adds together exact what is in each string
+
+add space(s) and punctuation as needed 
+
+Example:
+
+"Hello" + "World" 
+
+Returns 
+HelloWorld
+
+Example 2: 
+"Hello, " + "There World!" 
+
+Returns 
+Hello, There World!
+
+these are all examples of string concatenation in JavaScript 
+
+*/
