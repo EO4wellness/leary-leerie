0b18cde5561dd909f00b45cd0dc4a28bed88654c
EO4wellness <eo4wellness1122617@gmail.com>
2021-02-11 11:31:38 -0600

    Create convert-C-to-F.js

---
+++ JavaScript/code samples/convert-C-to-F.js
@@ -0,0 +1,29 @@
+/*
+ * Programming Quiz: Converting Tempatures (2-2)
+ *
+ * The Celsius-to-Fahrenheit formula:
+ *
+ *    F = C x 1.8 + 32
+ *
+ * 1. Set the fahrenheit variable to the correct value using the celsius variable and the forumla above
+ * 2. Log the fahrenheit variable to the console
+ *
+ */
+ 
+/*
+ * QUIZ REQUIREMENTS
+ * 1. Your code should have a variable `celsius`
+ * 2. You code should have a variable `fahrenheit`
+ * 3. Your variable `celsius` should equal `12`
+ * 4. Your variable `fahrenheit` should produce the output equal `53.6`
+ * 5. Your variable `fahrenheit` declaration should use the `celsius` variable
+ * 6. Your variable `fahrenheit` should have the correct formula
+ * 7. Your code should log the `fahrenheit` variable
+ * 8. Your code should not be empty
+ */
+ 
+
+var celsius = 12;
+var fahrenheit = celsius * 1.8 + 32;
+
+console.log(fahrenheit);
